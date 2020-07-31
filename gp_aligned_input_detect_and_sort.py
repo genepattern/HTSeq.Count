0dbdc36946068c6bfb1f88af98ce1a45ffd8801c
bahill <bhill@broadinstitute.org>
2020-07-30 23:19:48 -0400

    Updating comments and first attempt at argparse

--- gp_aligned_input_detect_and_sort.py
+++ gp_aligned_input_detect_and_sort.py
@@ -2,25 +2,43 @@
 
 import os
 import pysam
+import argparse
 
-input_filename = 
+parser = argparse.ArgumentParser()
+parser.add_argument("-", "--input", 
+	                dest="alignment_file",
+	                default = None,
+                    type=str,
+                    help="Input file(s) in SAM or BAM format.")
+parser.parse_args()
 
-# check to see if input appears to be intact & print files names that don't pass to stdout
+# ~~~need to make this all a loop for all inputs provided~~
+alignment_file =
+
+# check to see if input appears to be intact 
+# & print files names that don't pass to stdout
 # !does not read the middle of the file!
 # see samtools doc for more information
-pysam.quickcheck("-v", "manifest")
+pysam.quickcheck("-v", alignment_file)
 print('quickchecked')
 
-# file extenstion check/format detection
-# This can be removed in a future version when htSeq-count no longer requires a paramter it is ignoring...
-filename, file_extension = os.path.splitext('/path/to/somefile.ext')
+# file extension check/format detection
+# This can be removed in a future version when 
+# htseq-count no longer requires a parameter it is ignoring...
+filename, file_extension = os.path.splitext(alignment_file)
 print(filename)
 print(file_extension)
-#send file_extension to the other wrapper.
+#send file_extension to the other wrapper as the format variable
 
-pysam.sort("-o", "SRR1039508.mapped.Pos_sorted.bam", "-n", "SRR1039508.mapped.Pos_sorted.bam")
+#samtools sort [-l level] [-m maxMem] [-o out.bam] [-O format] [-n] [-t tag] [-T tmpprefix] [-@ threads] [in.sam|in.bam|in.cram]
+pysam.sort(
+    "-o", 
+    input_filename + "_nameSort" + file_extension,
+    "-n", 
+    filename)
 print('sorted')
 
 # TO DO
 # 7.15.20 - need variables for input.filename & out - set these at the top and reuse throughout
 # need to pass values to htseq_count_wrapper
+ 
