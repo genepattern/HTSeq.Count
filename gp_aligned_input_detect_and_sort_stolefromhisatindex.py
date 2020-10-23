5cab586bba0bb8580bf72ec883dadeca0f725bf1
bahill <bhill@broadinstitute.org>
2020-10-23 15:59:46 -0400

    First final attempt at new preprocess wrapper

--- gp_aligned_input_detect_and_sort_stolefromhisatindex.py
+++ gp_aligned_input_detect_and_sort_stolefromhisatindex.py
@@ -12,89 +12,103 @@ import pysam
 # commandLine=perl /htseq_count/htseq_count_wrapper.pl --htseqcount htseq-count --input <input.files> <sample.names> --gtf <GTF.file> --strand <strandedness> --minqual <min.qual> --mode <mode> --nonunique <count.nonunique> --secondary <count.secondary> --supplementary <count.supplementary> --featuretype <feature.type> --idtype <id.type> <gene.name> --output <output.file> --outformat <output.format>
 
 def generate_command():
-    buff = StringIO()
-    buff.write(u"htseq-count ")
+        buff = StringIO()
+        buff.write(u"htseq-count ")
 
-    allargs = iter(sys.argv[1:])
-    for arg in allargs:
-        # NOTE need to return the array to use globally
-        if (arg.startswith('--input'))
-            # should be a file list
-            val = next(allargs, None)
-            buff.write(u" ")
+        allargs = iter(sys.argv[1:])
+        for arg in allargs:
+            # NOTE need to return the array to use globally
+            if (arg.startswith('--input'))
+                # should be a file list
+                val = next(allargs, None)
+                buff.write(u" ")
 
-            with open(val) as f:
-                content = f.readlines()
-            # you may also want to remove whitespace characters like `\n` at the end of each line
-            content = [alignfile.strip() for alignfile in content]
+                with open(val) as f:
+                    content = f.readlines()
+                # you may also want to remove whitespace characters like `\n` at the end of each line
+                content = [alignfile.strip() for alignfile in content]
 
-            # check to see if input appears to be intact & print files names that don't pass to stdout
-            # !does not read the middle of the file! see samtools doc for more information
-            print("running samtools quickcheck")
-            for alignfile in content:
-                try:
-                    pysam.quickcheck("-v", alignfile)
-                except:
-                    print("One or more files is not intact. See below for more information")
-                else:
-                    print("quickcheck okay")
-
-                # file extension check/format detection
-                # This can be removed in a future version when
-                # htseq-count no longer requires a parameter it is ignoring...
-                print("determining file extension")
-                exts = [os.path.splitext(alignfile)[1]]
-                #file_extension = os.path.splitext(alignfile)[1]
-
-                # check that all extensions are the same
-                extensions = exts
-                check = True
-
-                for x in exts:
-                    if extensions != x:
-                        check = False
-                        break
-
-                    if check:
-                        print("All extensions are the same")
-                        file_extension = x
-                        print("input is " + file_extension)
-                        return file_extension
-
+                # check to see if input appears to be intact & print files names that don't pass to stdout
+                # !does not read the middle of the file! see samtools doc for more information
+                print("running samtools quickcheck")
+                for alignfile in content:
+                    try:
+                        pysam.quickcheck("-v", alignfile)
+                    except:
+                        print("One or more files is not intact. See below for more information")
                     else:
-                        print("Files are not of the same format")
-                        return
+                        print("quickcheck okay")
 
+                    # file extension check/format detection
+                    # This can be removed in a future version when
+                    # htseq-count no longer requires a parameter it is ignoring...
+                    print("determining file extension")
+                    exts = [os.path.splitext(alignfile)[1]]
 
+                    # check that all extensions are the same
+                    all_extensions = exts
+                    check = True
 
-                # samtools sort [-l level] [-m maxMem] [-o out.bam] [-O format]
-                # [-n] [-t tag] [-T tmpprefix] [-@ threads] [in.sam|in.bam|in.cram]
-                sorted_input = pysam.sort(
-                    "-o",
-                    alignfile + "_nameSort" + file_extension,
-                    "-n",
-                    alignfile)
-                print('sorted')
-                print("sorted file name is " + alignfile + "_nameSort" + file_extension)
+                    for x in exts:
+                        if all_extensions != x:
+                            check = False
+                            print("Files are not of the same format")
+                            break
 
-                return file_extension
+                        if check:
+                            print("All extensions are the same")
+                            file_extension = exts[1]
+                            print("input is " + file_extension)
+                            buff.write(u"--format")
+                            buff.write(unicode(file_extension))
 
+                            return file_extension
 
-            # need to check that the file extensions match ==
-            # need to write the sorted files back into the expected GP file list
+                    # samtools sort [-l level] [-m maxMem] [-o out.bam] [-O format]
+                    # [-n] [-t tag] [-T tmpprefix] [-@ threads] [in.sam|in.bam|in.cram]
+                    sorted_input = [pysam.sort(
+                        "-o",
+                        alignfile + "_nameSort" + file_extension,
+                        "-n",
+                        alignfile)]
+                    print('sorted')
+                    print("sorted file name is " + alignfile + "_nameSort" + file_extension)
+                    buff.write(u"--order name")
+
+                    return sorted_input
+
+                # write the sorted files back into the expected GP file list
+                f = open('input.files.list', 'w')
+                for ele in sorted_input:
+                    f.write(ele + '\n')
+                f.close()
+
+                buff.write(u"--input input.files.list")
 
 
 # get the original command line
 orig_cmd = str(sys.argv[1:])
 print("Original command line was " + orig_cmd)
+
 # Make new command line with sorted_input
 # & file_extension converted to the default value for format on the new command line.
-# new_cmd =
-# run htseq-count via original VIB perl wrapper with sorted file from this wrapper and format preset
-# WHAT IS HAPPENING??
-htseq_call = subprocess.Popen(["perl", "htseq_count_wrapper.pl", "--htseqcount htseq-count"], stdout=sys.stdout)
-htseq_call.communicate()
+#allargs = iter(sys.argv)
+#next(allargs)
+new_cmd = generate_command()
+print(new_cmd)
 
-# TO DO
-# add file_extension to the other wrapper as the format variable
-# sys.argv start at arg1 (not arg0) (or args.append? or parser.add.argument? - see opencravat)
+childProcess = subprocess.Popen(new_cmd, shell=True, env=os.environ, stdout=PIPE, stderr=PIPE)
+stdout, stderr = childProcess.communicate()
+retval = childProcess.returncode
+if retval != 0:
+    # if non-zero return, print stderr to stderr
+    print(stdout)
+    print >> sys.stderr, stderr
+#else:
+    # if not a non-zero stdout, print stderr to stdout since Hisat2Indexer logs non-error
+    # stuff to stderr.  Downside is the stderr and stdout are not interlevened
+    #print(stdout)
+    #print(stderr)
+
+sys.exit(retval)
+
