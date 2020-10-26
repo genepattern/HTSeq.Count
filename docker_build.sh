916193099538293fa9954bc5e526553e08cacf21
bahill <bhill@broadinstitute.org>
2020-10-26 13:13:40 -0400

    updated docker build script

--- docker_build.sh
+++ docker_build.sh
@@ -1,1 +1,2 @@
-docker build --rm https://github.com/genepattern/HTSeq.Count.git#develop -f Dockerfile -t htseq_count
+# remember to update the tag version
+docker build --rm https://github.com/genepattern/HTSeq.Count.git#develop -f Dockerfile -t genepattern/htseq_count:0.2
