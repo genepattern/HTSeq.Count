85447ed3c902920957d6e504306aba9d982b8e57
bahill <bhill@broadinstitute.org>
2021-01-05 14:45:16 -0500

    update to reflect build

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,2 @@
 # remember to update the tag version here and in your manifest
-docker build --rm https://github.com/genepattern/HTSeq.Count.git#develop -f Dockerfile -t genepattern/htseq_count:beta
+docker build --rm https://github.com/genepattern/HTSeq.Count.git#develop -f Dockerfile -t genepattern/htseq_count:debug
